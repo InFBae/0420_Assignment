e25ea49ec20ecc50016b9ed389c205c6ae302332
Jung BoBae <acarid27@gmail.com>
2023-04-20 21:11:06 +0900

    feat: Add Sort and 반복자 기술면접 준비 docs
    Sort 함수 구현 및 테스트코드 추가
    반복자 기술면접 준비 파일 추가

--- 0420_Assignment/Program.cs
+++ 0420_Assignment/Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+            /*
             DataStructure.List<int> list1 = new DataStructure.List<int>();
             DataStructure.LinkedList<int> list2 = new DataStructure.LinkedList<int>();
             for(int i = 0; i < 3; i++)
@@ -11,8 +12,7 @@
                 list1.Add(i);
                 list2.AddLast(i);
             }
-
-            foreach(int i in list1)
+            foreach (int i in list1)
             {
                 Console.WriteLine(i);
             }
@@ -20,7 +20,23 @@
             {
                 Console.WriteLine(i);
             }
+            */
 
+            int[] list1 = { 3, 2, 1 };
+            List<int> list2 = new List<int>() { 3, 2, 1 };
+
+            Sort(list1);
+            Sort(list2);
+            foreach (int i in list1) Console.WriteLine(i);
+            foreach (int i in list2) Console.WriteLine(i);
         }
+        
+        public static IEnumerable<T> Sort<T>(IEnumerable<T> source) where T: IComparable<T>
+        {
+            var list = source.ToList();
+            list.Sort();
+            return list;
+        }
+
     }
 }
