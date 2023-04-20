491e3a52298ac288029cd66b2fa757bea62c3586
Jung BoBae <acarid27@gmail.com>
2023-04-20 21:35:01 +0900

    feat: Add Average Function
    Average 함수 추가 및 교수님 코드 추가

--- 0420_Assignment/Program.cs
+++ 0420_Assignment/Program.cs
@@ -26,9 +26,11 @@
             List<int> list2 = new List<int>() { 3, 2, 1 };
 
             Sort(list1);
-            Sort(list2);
+            //Sort(list2, (a, b) =>  a > b);
             foreach (int i in list1) Console.WriteLine(i);
             foreach (int i in list2) Console.WriteLine(i);
+
+            Console.WriteLine(Average(list1));
         }
         
         public static IEnumerable<T> Sort<T>(IEnumerable<T> source) where T: IComparable<T>
@@ -36,6 +38,33 @@
             var list = source.ToList();
             list.Sort();
             return list;
+        }
+
+        public static void Sort<T>(IList<T> list, Comparison<T> comparer)
+        {
+            if (list == null) throw new ArgumentNullException(nameof(list));
+            for(int i = 0; i < list.Count; i++)
+            {
+                for(int j = i; j < list.Count; j++)
+                {
+                    if (comparer(list[j], list[j+1]) > 0)
+                    {
+                        T temp = list[j];
+                        list[j] = list[j+1];
+                        list[j+1] = temp;
+                    }
+                }
+            }
+        }
+
+        public static double Average(IEnumerable<int> ints)
+        {
+            double sum = 0;
+            foreach(int i in ints)
+            {
+                sum += i;
+            }
+            return sum/ints.Count();
         }
 
     }
