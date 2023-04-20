0bd4d52c51a139288e3e7d2ae501dcb5f1e594c1
Jung BoBae <acarid27@gmail.com>
2023-04-20 20:16:12 +0900

    feat: Add Enumerable at List and LinkedLsit
    List 와 LinkedLIst에 반복자 구현 및 테스트 코드 작성

--- 0420_Assignment/Program.cs
+++ 0420_Assignment/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            DataStructure.List<int> list1 = new DataStructure.List<int>();
+            DataStructure.LinkedList<int> list2 = new DataStructure.LinkedList<int>();
+            for(int i = 0; i < 3; i++)
+            {
+                list1.Add(i);
+                list2.AddLast(i);
+            }
+
+            foreach(int i in list1)
+            {
+                Console.WriteLine(i);
+            }
+            foreach(int i in list2)
+            {
+                Console.WriteLine(i);
+            }
+
         }
     }
 }
